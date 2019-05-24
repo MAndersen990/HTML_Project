98c94bc2a961c7984c0007e2da61be803a8b84c7
Andersen <MichaelAndersen@umms.umm.edu>
2019-05-24 14:37:29 -0400

    Removed my username and password for the database so the user can use their own

--- php/store.php
+++ php/store.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "localhost";
-	$username = "";
-	$password = "";
+	$username = '';
+	$password = '';
 	$dbname = "project2";
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
